3bb55f99a300074a1ab4fe4dbf5a8391b5c3a879
audckswl-prog <audckswl@gmail.com>
2025-10-17 01:58:54 +0900

    refactor: Adjust wobbly icon design

--- src/components/WobblyCommentIcon.tsx
+++ src/components/WobblyCommentIcon.tsx
@@ -21,24 +21,13 @@ const WobblyCommentIcon: React.FC<WobblyCommentIconProps> = ({ onClick, style })
           position: relative;
           width: 28px;
           height: 28px;
-          border: 2px solid black;
+          border: 1px solid black;
           border-radius: 73% 27% 59% 41% / 61% 57% 43% 39%;
           cursor: pointer;
           display: flex;
           justify-content: center;
           align-items: center;
           gap: 2px;
-        }
-        .wobbly-comment-icon::after {
-          content: '';
-          position: absolute;
-          bottom: -8px;
-          left: 4px;
-          width: 0;
-          height: 0;
-          border: 8px solid transparent;
-          border-top-color: black;
-          transform: rotate(20deg);
         }
         .wobbly-comment-icon-dot {
             width: 4px;
